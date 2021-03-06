23dbee88382b04d56b026cd4ba03b1f36a3853cb
remigiuszkrol <remigiusz.krol93@gmail.com>
2021-03-06 14:41:30 +0100

    ~ rekrol - MiniJIRA - ClientApplication

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace MiniJIRAClientApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(137, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(537, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxId.Location = new System.Drawing.Point(138, 29);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(233, 29);
+            this.textBoxId.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSearch.Location = new System.Drawing.Point(386, 29);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(96, 29);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Wyszukaj";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace MiniJIRAClientApplication
+{
+    public partial class Form1 : Form
+    {
+        MiniJIRA.MainMiniJIRA proxy = new MiniJIRA.MainMiniJIRA();
+        HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WebServicesSettings()
+        {
+            client.BaseAddress = new Uri("https://localhost:44394/MainMiniJIRA.asmx/");
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string usersJson = proxy.UsersTable();
+            //DataTable dtUsers = JsonConvert.DeserializeObject<DataTable>(usersJson);
+            //dataGridView1.DataSource = dtUsers;
+
+            WebServicesSettings();
+        }
+
+        private DataTable stringSplit(string userJson)
+        {
+            string[] json = userJson.Split('>');
+            string[] finalJson = json[2].Split('<');
+
+            DataTable dt = JsonConvert.DeserializeObject<DataTable>(finalJson[0]);
+            return dt;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Nie uzywaj dodatkowych spacji!!!
+            HttpResponseMessage message = client.GetAsync("UsersTableForUsers?id=" + textBoxId.Text + "").Result;
+            string userJson = message.Content.ReadAsStringAsync().Result;
+            dataGridView1.DataSource = stringSplit(userJson);
+        }
+    }
+}
